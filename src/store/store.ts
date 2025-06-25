792b817d954f6926520a231358eb9267314c408a
jimbdy <jim@bdy.tech>
2025-06-25 05:37:41 -0700

    fix: 修复中间件格式化问题

--- src/store/store.ts
+++ src/store/store.ts
@@ -33,7 +33,7 @@ export const store = configureStore({
     blogReducer: BlogReducer,
   },
   devTools: process.env.NODE_ENV !== "production",
-  middleware: (getDefaultMiddleware) =>
+  middleware: (getDefaultMiddleware) => 
     getDefaultMiddleware({ serializableCheck: false, immutableCheck: false }),
 });
 
