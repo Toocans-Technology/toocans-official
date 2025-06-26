978c6987247c6c161239bb1e83e7d798eeeb4b74
Chacha <chacha@bdy.tech>
2025-06-25 23:50:33 -0700

    feat: remove useless code

--- src/app/account/components/NicknameDialog/types.ts
+++
@@ -1,7 +0,0 @@
-export interface NicknameDialogProps {
-  open: boolean;
-  onClose: () => void;
-  currentNickname: string;
-  onSave: (newNickname: string) => Promise<void>;
-  isUpdating: boolean;
-}
