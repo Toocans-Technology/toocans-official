801a743f62d5390c70dc69fcc2635605da6c43c7
Chacha <chacha@bdy.tech>
2025-06-26 03:08:49 -0700

    feat: update env utils

--- apps/web/lib/utils/env.ts
+++ apps/web/lib/utils/env.ts
@@ -2,7 +2,7 @@ export const isDevEnvironment = () => process.env.NEXT_PUBLIC_ENV_NAME === 'deve
 
 export const isTestingEnvironment = () =>
   process.env.NEXT_PUBLIC_ENV_NAME === 'dev' ||
-  process.env.NEXT_PUBLIC_ENV_NAME === 'pre' ||
+  process.env.NEXT_PUBLIC_ENV_NAME === 'uat' ||
   process.env.NEXT_PUBLIC_ENV_NAME === 'test'
 
 export const isOnlineEnvironment = () => process.env.NEXT_PUBLIC_ENV_NAME === 'production'
