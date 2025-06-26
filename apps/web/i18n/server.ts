b16e40e95ff41f32478b8b995b42b5e30d677b17
Chacha <chacha@bdy.tech>
2025-06-26 19:08:33 +0800

    feat: update i18n function types

--- apps/web/i18n/server.ts
+++ apps/web/i18n/server.ts
@@ -1,4 +1,4 @@
-import { createInstance } from 'i18next'
+import { createInstance, i18n, TFunction } from 'i18next'
 import resourcesToBackend from 'i18next-resources-to-backend'
 import { initReactI18next } from 'react-i18next/initReactI18next'
 import 'server-only'
@@ -14,7 +14,7 @@ const initI18next = async (lang: string, ns?: Namespace) => {
   return i18nInstance
 }
 
-export async function getT(lang: string, ns?: Namespace) {
+export async function getT(lang: string, ns?: Namespace): Promise<{ t: TFunction; i18n: i18n }> {
   const i18nextInstance = await initI18next(lang, ns)
   return {
     t: i18nextInstance.getFixedT(lang, Array.isArray(ns) ? ns[0] : ns),
