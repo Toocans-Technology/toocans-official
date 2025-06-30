c4a497b62ad6a06247cf9896fd1ed4a1085a070d
Chacha <chacha@bdy.tech>
2025-06-30 11:40:39 +0800

    feat: add BaseProviders and providers folder

---
+++ apps/web/components/providers/BaseProviders.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import { QueryClientProvider } from '@tanstack/react-query'
+import { ThemeProvider as NextThemesProvider } from 'next-themes'
+import * as React from 'react'
+import { getQueryClient } from '@/lib/utils/queryClient'
+
+export function BaseProviders({ children }: { children: React.ReactNode }) {
+  const queryClient = getQueryClient()
+
+  return (
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+      enableColorScheme
+    >
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </NextThemesProvider>
+  )
+}
