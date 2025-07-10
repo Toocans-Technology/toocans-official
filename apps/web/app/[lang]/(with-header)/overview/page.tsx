0b3462c615ba58f5f84e5996b2f22967c25514a8
Chacha <chacha@bdy.tech>
2025-07-10 11:14:48 +0800

    fix: fix build errors

--- apps/web/app/[lang]/(with-header)/overview/page.tsx
+++ apps/web/app/[lang]/(with-header)/overview/page.tsx
@@ -1,14 +1,13 @@
 import OverviewBalancePanel from '@/components/overview/OverviewBalancePanel'
 import TokenTable from '@/components/overview/TokenTable'
 
-export default function Page({ params }: { params: { lang: string } }) {
-  const { lang } = params
+export default function Page() {
   return (
-    <div className="min-h-screen bg-[#f6f6f6] flex justify-center">
+    <div className="flex min-h-screen justify-center bg-[#f6f6f6]">
       <div className="w-full max-w-[942px] px-8 pt-[60px]">
-        <h1 className="mb-[46px] text-black font-inter text-[32px] font-medium leading-[30px]">Asset Overview</h1>
+        <h1 className="font-inter mb-[46px] text-[32px] font-medium leading-[30px] text-black">Asset Overview</h1>
         <OverviewBalancePanel />
-        <TokenTable lang={lang} />
+        <TokenTable />
       </div>
     </div>
   )
