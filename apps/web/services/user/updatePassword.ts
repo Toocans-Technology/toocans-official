5a04cb51093bc9f9cb15ec02590b7e09c1f0050c
Chacha <chacha@bdy.tech>
2025-07-10 10:31:38 +0800

    feat: add updatePassword api request

---
+++ apps/web/services/user/updatePassword.ts
@@ -0,0 +1,30 @@
+import { useMutation } from '@tanstack/react-query'
+import { z } from 'zod'
+import { getMutation } from '@/lib/api'
+import { getUrl } from '@/lib/api/getUrl'
+
+export const UpdatePasswordParamsSchema = z.object({
+  password: z.string().min(8).max(32),
+  oldPassword: z.string().min(8).max(32),
+  code: z.string().min(6).max(6),
+})
+
+export type UpdatePasswordParams = z.infer<typeof UpdatePasswordParamsSchema>
+
+export const UpdatePasswordResponseSchema = z.nullable(z.boolean())
+
+export type UpdatePasswordResponse = z.infer<typeof UpdatePasswordResponseSchema>
+
+export const useUpdatePassword = () => {
+  return useMutation(
+    getMutation((params: UpdatePasswordParams) => {
+      return {
+        method: 'POST',
+        url: getUrl('/user/updatePassword'),
+        query: UpdatePasswordParamsSchema.parse(params),
+        body: {},
+        transfer: UpdatePasswordResponseSchema.parse,
+      }
+    })
+  )
+}
