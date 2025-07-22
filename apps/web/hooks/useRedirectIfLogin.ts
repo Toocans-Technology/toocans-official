06d4e5662c7b91a913ec00a700221be24a105737
Chacha <chacha@bdy.tech>
2025-07-22 11:27:18 +0800

    feat: update redirect issue

--- apps/web/hooks/useRedirectIfLogin.ts
+++ apps/web/hooks/useRedirectIfLogin.ts
@@ -5,14 +5,16 @@ import { useLogin } from './useLogin'
 import { useNoLocalePathname } from './useNoLocalePathname'
 import { useRouter } from './useRouter'
 
+const OVERVIEW_PATHNAME = '/overview'
+
 export const useRedirectIfLogin = () => {
   const { isLoggedIn } = useLogin()
   const router = useRouter()
   const pathname = useNoLocalePathname()
   const [from] = useQueryState('from')
-  const redirectPathname = from || '/'
+  const redirectPathname = from || OVERVIEW_PATHNAME
 
-  if (isLoggedIn && pathname !== '/') {
+  if (isLoggedIn && pathname !== OVERVIEW_PATHNAME) {
     router.replace(redirectPathname)
   }
 
