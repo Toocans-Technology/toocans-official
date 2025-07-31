db5d6a5e0baf9612520b4bf864ebe09ab5afc683
jimbdy <jim@bdy.tech>
2025-07-31 15:18:08 +0800

    feat: 更新AuthApp页面布局，调整样式和结构以改善用户体验

--- apps/web/app/[lang]/(with-header)/overview/page.tsx
+++ apps/web/app/[lang]/(with-header)/overview/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
   return (
     <div className="flex min-h-screen justify-center bg-[#f6f6f6]">
       <div className="w-full max-w-[942px] px-8 pt-[60px]">
-        <h1 className="font-inter mb-[16px] text-[32px] font-medium leading-[30px] text-black">
+        <h1 className="font-inter mb-[18px] text-[32px] font-medium leading-[30px] text-black">
           {t('overview:AssetOverview')}
         </h1>
         <OverviewBalancePanel />
