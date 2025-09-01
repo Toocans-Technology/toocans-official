7957fbd5a8364f41c19d093fd0f7229b6560d35e
jimbdy <jim@bdy.tech>
2025-09-02 07:44:33 +0800

    feat: 移除Markets组件，简化代码结构

--- apps/web/components/home/Markets.tsx
+++ apps/web/components/home/Markets.tsx
@@ -1,21 +1,6 @@
 'use client'
 
 import Image from 'next/image'
-import { FunctionComponent } from 'react'
-
-const Markets: FunctionComponent = () => {
-  return (
-    <div className="w-full bg-[#0f0f0f]">
-      <div className="mx-auto flex max-w-[1200px] items-center justify-between px-8 py-16 text-white">
-        <Image src="/images/home/markets.png" alt="Markets" width={1200} height={540} />
-      </div>
-    </div>
-  )
-}
-
-export default Markets
-
-import Image from 'next/image'
 import { useId } from 'react'
 import { FunctionComponent, useEffect, useMemo, useState } from 'react'
 import { useAllToken } from '@/hooks/useAllToken'
